111117c1ee6301e493226a11854b9adc5d772add
Vinicius Nishida <viniciusnishida13@gmail.com>
2024-11-03 19:15:55 -0300

    initial commit

---
+++ MyPatches/SimplePatch.cs
@@ -0,0 +1,51 @@
+﻿using EFT;
+using Comfort.Common;
+using System.Reflection;
+using HarmonyLib;
+using SPT.Reflection.Patching;
+
+
+namespace makeMedsGreatAgain.MyPatches
+{
+    internal class SimplePatch : ModulePatch // all patches must inherit ModulePatch
+    {
+ 
+        internal struct PlayerInfo
+        {
+            internal static GameWorld gameWorld
+            { get => Singleton<GameWorld>.Instance; }
+
+            internal static Player.FirearmController FC
+            { get => player.HandsController as Player.FirearmController; }
+
+            internal static Player player
+            { get => gameWorld.MainPlayer; }
+
+        }
+
+        
+        protected override MethodBase GetTargetMethod()
+        {
+            return AccessTools.Method(typeof(MovementContext), nameof(MovementContext.SetPhysicalCondition));
+        }
+
+        [PatchPrefix]
+        static bool Prefix(MovementContext __instance, EPhysicalCondition c, ref bool __result)
+        {
+            // code in Prefix() method will run BEFORE original code is executed.
+            // if 'true' is returned, the original code will still run.
+            // if 'false' is returned, the original code will be skipped.
+
+            if (c == EPhysicalCondition.HealingLegs && Plugin.canWalkInSurgery.Value)
+            {
+                __result = false; 
+                return false; 
+            }
+
+            return true; 
+
+        }
+
+    }
+
+}
---
+++ Plugin.cs
@@ -0,0 +1,28 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using BepInEx.Configuration;
+using makeMedsGreatAgain.MyPatches;
+
+namespace makeMedsGreatAgain
+{
+    // first string below is your plugin's GUID, it MUST be unique to any other mod. Read more about it in BepInEx docs. Be sure to update it if you copy this project.
+    [BepInPlugin("com.vinihns.makeMedsGreatAgain", "Make Meds Great Again!", "1.0.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+        public static ManualLogSource LogSource;
+        public static ConfigEntry<bool> canWalkInSurgery;
+
+        // BaseUnityPlugin inherits MonoBehaviour, so you can use base unity functions like Awake() and Update()
+        private void Awake()
+        {
+            // save the Logger to variable so we can use it elsewhere in the project
+            LogSource = Logger;
+            LogSource.LogInfo("Adding little things");
+
+            canWalkInSurgery = Config.Bind("Config", "Can walk in surgery", true, "Boolean setting description");
+            
+            // uncomment line(s) below to enable desired example patch, then press F6 to build the project:
+            new SimplePatch().Enable();
+        }
+    }
+}
