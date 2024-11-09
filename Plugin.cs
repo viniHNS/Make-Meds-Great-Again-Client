2f30672ece1e15b080ecbe96c3df9430cc20441e
Vinicius Nishida <viniciusnishida13@gmail.com>
2024-11-09 11:18:24 -0300

    Update plugin version and add new configuration options for sprinting and surgery

--- Plugin.cs
+++ Plugin.cs
@@ -6,11 +6,12 @@ using makeMedsGreatAgain.MyPatches;
 namespace makeMedsGreatAgain
 {
     // first string below is your plugin's GUID, it MUST be unique to any other mod. Read more about it in BepInEx docs. Be sure to update it if you copy this project.
-    [BepInPlugin("com.vinihns.makeMedsGreatAgain", "Make Meds Great Again!", "1.0.0")]
+    [BepInPlugin("com.vinihns.makeMedsGreatAgain", "Make Meds Great Again!", "1.1.0")]
     public class Plugin : BaseUnityPlugin
     {
         public static ManualLogSource LogSource;
         public static ConfigEntry<bool> canWalkInSurgery;
+        public static ConfigEntry<bool> canSprintUsingMeds;
 
         // BaseUnityPlugin inherits MonoBehaviour, so you can use base unity functions like Awake() and Update()
         private void Awake()
@@ -19,10 +20,12 @@ namespace makeMedsGreatAgain
             LogSource = Logger;
             LogSource.LogInfo("Adding little things");
 
-            canWalkInSurgery = Config.Bind("Config", "Can walk in surgery", true, "Boolean setting description");
+            canWalkInSurgery = Config.Bind("Config", "Can walk in surgery", true, "Allows the player to walk while performing surgical treatments.");
+            canSprintUsingMeds = Config.Bind("Config", "Can sprint using meds", true, "Allows sprinting while using meds—because pain can't always slow you down.");
             
             // uncomment line(s) below to enable desired example patch, then press F6 to build the project:
-            new SimplePatch().Enable();
+            new surgeryPatch().Enable();
+            new runPatch().Enable();
         }
     }
 }
