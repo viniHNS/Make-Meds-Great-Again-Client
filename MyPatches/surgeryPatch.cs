d521cd833379152dc205f30c8279402166e90945
Vinicius Nishida <viniciusnishida13@gmail.com>
2024-11-09 11:19:44 -0300

    Add surgeryPatch to modify behavior

---
+++ MyPatches/surgeryPatch.cs
@@ -0,0 +1,37 @@
+﻿using EFT;
+using Comfort.Common;
+using System.Reflection;
+using HarmonyLib;
+using SPT.Reflection.Patching;
+
+
+namespace makeMedsGreatAgain.MyPatches
+{
+    internal class surgeryPatch : ModulePatch // all patches must inherit ModulePatch
+    {
+ 
+        protected override MethodBase GetTargetMethod()
+        {
+            return AccessTools.Method(typeof(MovementContext), nameof(MovementContext.SetPhysicalCondition));
+        }
+
+        [PatchPrefix]
+        static bool Prefix(EPhysicalCondition c, ref bool __result)
+        {
+            // code in Prefix() method will run BEFORE original code is executed.
+            // if 'true' is returned, the original code will still run.
+            // if 'false' is returned, the original code will be skipped.
+
+            if (c == EPhysicalCondition.HealingLegs && Plugin.canWalkInSurgery.Value)
+            {
+                __result = false; 
+                return false; 
+            }
+
+            return true; 
+
+        }
+
+    }
+
+}
