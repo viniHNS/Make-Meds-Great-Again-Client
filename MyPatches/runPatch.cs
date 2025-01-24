c84df80b9f827490e9b4504f595c9ee0b9341c24
Vinicius Hissayoshi Nishida <viniciusnishida13@gmail.com>
2025-01-24 20:31:27 -0300

    Enhance runPatch to include player instance check in Prefix method

--- MyPatches/runPatch.cs
+++ MyPatches/runPatch.cs
@@ -1,5 +1,4 @@
 ﻿using EFT;
-using Comfort.Common;
 using System.Reflection;
 using HarmonyLib;
 using SPT.Reflection.Patching;
@@ -8,28 +7,30 @@ namespace makeMedsGreatAgain.MyPatches
 {
     internal class runPatch : ModulePatch // all patches must inherit ModulePatch
     {
-
         protected override MethodBase GetTargetMethod()
         {
             return AccessTools.Method(typeof(MovementContext), nameof(MovementContext.SetPhysicalCondition));
         }
 
         [PatchPrefix]
-        static bool Prefix(EPhysicalCondition c, ref bool __result)
+        static bool Prefix(EPhysicalCondition c, ref bool __result, Player ____player)
         {
-            // code in Prefix() method will run BEFORE original code is executed.
-            // if 'true' is returned, the original code will still run.
-            // if 'false' is returned, the original code will be skipped.
+            // If this player instance is not the main player, don't continue the rest of the method
+            if (!____player.IsYourPlayer || PlayerInfo.player is HideoutPlayer)
+            {
+                return true;
+            }
+            
+            Plugin.LogSource.LogWarning("_PLAYER => " + ____player.Profile.Info.Nickname);
 
             if (c == EPhysicalCondition.UsingMeds && Plugin.canSprintUsingMeds.Value)
             {
                 __result = false;
                 return false;
+                
             }
 
             return true;
-
         }
-
     }
 }
